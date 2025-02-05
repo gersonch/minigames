4f0ccafe01524b47809ad4ae5dfabdbb78af4d0d
Gerson Contreras <78560393+gersonch@users.noreply.github.com>
2025-02-05 00:34:03 -0300

    Update getWordOfTheDay.ts

--- src/lib/getWordOfTheDay.ts
+++ src/lib/getWordOfTheDay.ts
@@ -1,5 +1,5 @@
 export async function getWordOfTheDay() {
-  const response = await fetch("http://localhost:3000/wordOfTheDay");
+  const response = await fetch("https://minigames-back-qabrim6oo-gerson-contreras-projects.vercel.app/wordOfTheDay");
   const data = await response.json();
   return data;
 }
