5e12894c4ec77d657b852937ae7a15f55de70f88
gersonch <78560393+gersonch@users.noreply.github.com>
2025-02-05 19:36:31 -0300

    link fetch

--- src/lib/getWordOfTheDay.ts
+++ src/lib/getWordOfTheDay.ts
@@ -1,5 +1,7 @@
 export async function getWordOfTheDay() {
-  const response = await fetch("http://localhost:3000/wordOfTheDay");
+  const response = await fetch(
+    "https://minigames-back.vercel.app/wordOfTheDay"
+  );
   const data = await response.json();
   return data;
 }
