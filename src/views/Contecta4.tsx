7fadcfd2d9fee137317122f4f7f14a76d154599e
gersonch <78560393+gersonch@users.noreply.github.com>
2025-03-04 23:15:29 -0300

    turns

--- src/views/Contecta4.tsx
+++ src/views/Contecta4.tsx
@@ -1,9 +1,9 @@
 import { BoardConecta4 } from "../components/BoardConecta4";
 
-const Turns = {
-  player1: "rojo",
-  player2: "amarillo",
-};
+// const Turns = {
+//   player1: "rojo",
+//   player2: "amarillo",
+// };
 
 export function Conecta4() {
   return (
